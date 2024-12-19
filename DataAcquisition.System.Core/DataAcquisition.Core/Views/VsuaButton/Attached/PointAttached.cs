fc36314f4f93e575e598bb0d6aa8ee9c1d011bb8
Xioa <132796255@qq.com>
2024-12-19 17:40:14 +0800

    Attached

---
+++ DataAcquisition.System.Core/DataAcquisition.Core/Views/VsuaButton/Attached/PointAttached.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+
+namespace DataAcquisition.Core.Views.VsuaButton.Attached
+{
+    public static class PointAttached
+    {
+        public static readonly DependencyProperty PointProperty = DependencyProperty.RegisterAttached(
+        "Point",
+        typeof(Point),
+        typeof(PointAttached),
+        new PropertyMetadata(new Point(60,200)));
+
+        public static Point GetPoint(DependencyObject obj)
+        {
+            return (Point)obj.GetValue(PointProperty);
+        }
+
+        public static void SetPoint(DependencyObject obj, Point value)
+        {
+            obj.SetValue(PointProperty, value);
+        }
+
+       
+    }
+}
