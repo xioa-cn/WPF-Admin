ab7fdfa9cde2c42734a12f769163b6c26bf7e1ec
Xioa <132796255@qq.com>
2024-12-26 11:36:29 +0800

    优化SwitchWindow动画播放时 用户还可以点击界面Bug

--- DataAcquisition.System.Core/DataAcquisition.Core/WindowManager/WindowManager.cs
+++ DataAcquisition.System.Core/DataAcquisition.Core/WindowManager/WindowManager.cs
@@ -4,20 +4,18 @@ using System.Windows.Media.Animation;
 
 namespace DataAcquisition.Core.WindowManager;
 
-public static class WindowManager
-{
+public static class WindowManager {
     private const double AnimationDuration = 2000; // 动画持续时间(毫秒)
 
-    public static void SwitchWindow(this Window currentWindow, Window newWindow)
-    {
+    public static void SwitchWindow(this Window currentWindow, Window newWindow) {
+        currentWindow.IsEnabled = false;
         // 设置新窗口的初始状态
         newWindow.Opacity = 0;
         newWindow.Show();
         newWindow.WindowState = currentWindow.WindowState;
 
         // 创建淡出动画
-        var fadeOutAnimation = new DoubleAnimation
-        {
+        var fadeOutAnimation = new DoubleAnimation {
             From = 1,
             To = 0,
             Duration = TimeSpan.FromMilliseconds(AnimationDuration),
@@ -25,8 +23,7 @@ public static class WindowManager
         };
 
         // 创建淡入动画
-        var fadeInAnimation = new DoubleAnimation
-        {
+        var fadeInAnimation = new DoubleAnimation {
             From = 0,
             To = 1,
             Duration = TimeSpan.FromMilliseconds(AnimationDuration),
@@ -41,13 +38,11 @@ public static class WindowManager
         newWindow.BeginAnimation(UIElement.OpacityProperty, fadeInAnimation);
     }
 
-    public static void ShowWindowWithFade(this Window window)
-    {
+    public static void ShowWindowWithFade(this Window window) {
         window.Opacity = 0;
         window.Show();
 
-        var fadeInAnimation = new DoubleAnimation
-        {
+        var fadeInAnimation = new DoubleAnimation {
             From = 0,
             To = 1,
             Duration = TimeSpan.FromMilliseconds(AnimationDuration),
@@ -57,16 +52,14 @@ public static class WindowManager
         window.BeginAnimation(UIElement.OpacityProperty, fadeInAnimation);
     }
 
-    public static void CloseWindowWithFade(this Window window)
-    {
-        var fadeOutAnimation = new DoubleAnimation
-        {
+    public static void CloseWindowWithFade(this Window window) {
+        var fadeOutAnimation = new DoubleAnimation {
             From = 1,
             To = 0,
             Duration = TimeSpan.FromMilliseconds(AnimationDuration),
             EasingFunction = new CubicEase { EasingMode = EasingMode.EaseInOut }
         };
-        
+
         fadeOutAnimation.Completed += (s, e) => window.Close();
         window.BeginAnimation(UIElement.OpacityProperty, fadeOutAnimation);
     }
