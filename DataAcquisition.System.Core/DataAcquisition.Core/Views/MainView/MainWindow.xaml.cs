9e76980d5c719ea2df2fa8ff1a4a89265e1601a5
Xioa <132796255@qq.com>
2024-12-26 08:55:43 +0800

    NavButton 添加缩放动画 以及判断时double类型误差

--- DataAcquisition.System.Core/DataAcquisition.Core/Views/MainView/MainWindow.xaml.cs
+++ DataAcquisition.System.Core/DataAcquisition.Core/Views/MainView/MainWindow.xaml.cs
@@ -9,13 +9,13 @@ using DataAcquisition.Core.Views.NotificationView.Model;
 using HandyControl.Controls;
 using HandyControl.Tools.Extension;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace DataAcquisition.Core.Views.MainView;
 
-public partial class MainWindow : System.Windows.Window
-{
-    public MainWindow()
-    {
+public partial class MainWindow : System.Windows.Window {
+    public MainWindow() {
         InitializeComponent();
         Dialog.Register(MessageToken.DialogPageToken, this);
         WeakReferenceMessenger.Default.Register<ChangeMainBorderSizeMessanger>(this, ChangeValue);
@@ -25,18 +25,15 @@ public partial class MainWindow : System.Windows.Window
         };
     }
 
-    private void ChangeValue(object recipient, ChangeMainBorderSizeMessanger message)
-    {
+    private void ChangeValue(object recipient, ChangeMainBorderSizeMessanger message) {
         ChangeNavBorder();
     }
 
-    private void MiniSize_Click(object sender, RoutedEventArgs e)
-    {
+    private void MiniSize_Click(object sender, RoutedEventArgs e) {
         this.WindowState = WindowState.Minimized;
     }
 
-    private void MaxSize_Click(object sender, RoutedEventArgs e)
-    {
+    private void MaxSize_Click(object sender, RoutedEventArgs e) {
         if (this.WindowState == WindowState.Normal)
         {
             this.WindowState = WindowState.Maximized;
@@ -47,13 +44,14 @@ public partial class MainWindow : System.Windows.Window
             this.WindowState = WindowState.Normal;
         }
     }
+
     private NotifyIconView? _notifyIconView;
-    private async void Close_Click(object sender, RoutedEventArgs e)
-    {
+
+    private async void Close_Click(object sender, RoutedEventArgs e) {
         _notifyIconView ??= new NotifyIconView();
 
         var closeEnum = CloseEnum.None;
-        var dialog = Dialog.Show(_notifyIconView,MessageToken.DialogPageToken);
+        var dialog = Dialog.Show(_notifyIconView, MessageToken.DialogPageToken);
         await dialog.Initialize<NotifyIconViewModel>(
             vm => { }).GetResultAsync<CloseEnum>().ContinueWith(re => { closeEnum = re.Result; });
 
@@ -74,24 +72,23 @@ public partial class MainWindow : System.Windows.Window
         }
     }
 
-    private void OpenOrCloseNaviBar(object sender, RoutedEventArgs e)
-    {
+    private void OpenOrCloseNaviBar(object sender, RoutedEventArgs e) {
         ChangeNavBorder();
     }
 
 
-    private void ChangeNavBorder()
-    {
+    private void ChangeNavBorder() {
         var width = naviGrid.Width;
-        if (width == 55)
-        {
-            Icon_grid.Visibility = Visibility.Collapsed;
-            naviGrid.Width = 200;
-        }
-        else
-        {
-            Icon_grid.Visibility = Visibility.Visible;
-            naviGrid.Width = 55;
-        }
+        const double minWidth = 55.0;
+        const double maxWidth = 200.0;
+
+        DoubleAnimation doubleAnimation = new DoubleAnimation {
+            Duration = new Duration(TimeSpan.FromSeconds(0.1)),
+            From = width,
+            To = Math.Abs(width - minWidth) < 0.01 ? maxWidth : minWidth
+        };
+
+        Icon_grid.Visibility = Math.Abs(width - minWidth) < 0.01 ? Visibility.Collapsed : Visibility.Visible;
+        naviGrid.BeginAnimation(Border.WidthProperty, doubleAnimation);
     }
 }
