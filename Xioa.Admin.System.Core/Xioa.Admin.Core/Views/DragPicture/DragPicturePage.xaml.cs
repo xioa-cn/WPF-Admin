0d5734cbcc07918021426faf548925eec713142d
Xioa <132796255@qq.com>
2024-12-31 17:15:43 +0800

    优化检查逻辑

--- Xioa.Admin.System.Core/Xioa.Admin.Core/Views/DragPicture/DragPicturePage.xaml.cs
+++ Xioa.Admin.System.Core/Xioa.Admin.Core/Views/DragPicture/DragPicturePage.xaml.cs
@@ -17,7 +17,7 @@ public partial class DragPicturePage : Page
 
     private void Image_DragOver(object sender, DragEventArgs e)
     {
-        Debug.WriteLine("Drag over event triggered."); // Debug information
+        Debug.WriteLine("Drag over event triggered."); 
         if (e.Data.GetDataPresent(DataFormats.FileDrop))
         {
             e.Effects = DragDropEffects.Copy;
@@ -31,22 +31,20 @@ public partial class DragPicturePage : Page
 
     private void Image_Drop(object sender, DragEventArgs e)
     {
-        Debug.WriteLine("Drop event triggered."); // Debug information
-        var files = (string[])e.Data.GetData(DataFormats.FileDrop);
-        if (files != null && files.Length > 0)
+        Debug.WriteLine("Drop event triggered."); 
+        var files = (string[]?)e.Data.GetData(DataFormats.FileDrop);
+        if (files == null || files.Length <= 0) return;
+        string file = files[0];
+        string extension = System.IO.Path.GetExtension(file).ToLower();
+        var imageExtensions = new HashSet<string> { ".jpg", ".jpeg", ".png", ".gif", ".bmp" };
+        if (imageExtensions.Contains(extension))
         {
-            string file = files[0];
-            string extension = System.IO.Path.GetExtension(file).ToLower();
-            var imageExtensions = new HashSet<string> { ".jpg", ".jpeg", ".png", ".gif", ".bmp" };
-            if (imageExtensions.Contains(extension))
-            {
-                Debug.WriteLine($"Dropped file is an image: {file}"); // Debug information
-                displayImage.Source = new BitmapImage(new Uri(file));
-            }
-            else
-            {
-                Debug.WriteLine("Dropped file is not an image."); // Debug information
-            }
+            Debug.WriteLine($"Dropped file is an image: {file}"); 
+            displayImage.Source = new BitmapImage(new Uri(file));
+        }
+        else
+        {
+            Debug.WriteLine("Dropped file is not an image."); 
         }
     }
 }
