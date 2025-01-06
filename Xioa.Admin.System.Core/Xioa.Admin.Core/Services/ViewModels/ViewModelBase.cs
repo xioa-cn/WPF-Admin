9907fcf59600fe742c9e45775de3ca7abbeadeff
XIOA <1327916255@qq.com>
2025-01-06 17:26:56 +0800

    ViewModelBase

---
+++ Xioa.Admin.System.Core/Xioa.Admin.Core/Services/ViewModels/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Xioa.Admin.Core.Services.ViewModels;
+
+public partial class ViewModelBase : ObservableObject, IDisposable {
+   
+
+    private void ReleaseUnmanagedResources() {
+        // TODO 在此释放非托管资源
+    }
+
+    public void Dispose() {
+        ReleaseUnmanagedResources();
+        GC.SuppressFinalize(this);
+    }
+}
